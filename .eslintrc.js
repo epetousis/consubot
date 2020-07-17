a7e423f388d87695fa568027687f06dc1fd1bc64
Evan Petousis <evan@petousis.net>
2020-07-17 15:54:29 +1000

    Move if statements into switch, fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,18 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        tsconfigRootDir: __dirname,
-        project: ['./tsconfig.json'],
-    },
-    plugins: ['@typescript-eslint'],
-    extends: [
-        'airbnb-typescript',
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: [
+      './tsconfig.json',
+      './tsconfig.eslint.json',
     ],
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'airbnb-typescript',
+  ],
+  rules: {
+    'no-console': 'off',
+  },
 };
