fc7fa74693c6fb8f527f881e0d55eea5ddad7cc9
Evan Petousis <evan@petousis.net>
2020-08-17 00:23:13 +1000

    Add OAuth redirect link

--- src/bot.ts
+++ src/bot.ts
@@ -3,6 +3,8 @@ import http from 'http';
 import CommandHandler from './handler';
 import FightCommands from './fight';
 
+const clientId = process.env.BOT_CLIENT_ID ?? '';
+const permissions = 2048;
 const token = process.env.BOT_AUTH_TOKEN ?? '';
 
 const client = new Client();
@@ -25,6 +27,8 @@ client.on('message', (message) => {
 client.login(token);
 
 http.createServer((request, response) => {
-  response.writeHead(200);
-  response.end("i'm not a fucking web server", 'utf-8');
+  response.writeHead(302, {
+    Location: `https://discord.com/api/oauth2/authorize?client_id=${clientId}&scope=bot&permissions=${permissions}`,
+  });
+  response.end();
 }).listen(process.env.PORT || 4096);
