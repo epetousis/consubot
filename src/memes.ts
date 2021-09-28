a0802d3937af9954cb4f9846325e1a842491a2e2
Evan Petousis <evan@petousis.net>
2021-09-28 17:15:33 +1000

    Add reactions to react command description

--- src/memes.ts
+++ src/memes.ts
@@ -1,6 +1,15 @@
 import { CommandInteraction } from 'discord.js';
 import { SlashCommandBuilder } from '@discordjs/builders';
 
+enum Reaction {
+  away,
+  boobs,
+  looking,
+  respectfully,
+  shook,
+  sheesh,
+}
+
 async function sendHostImage(message: CommandInteraction, path: string) {
   const imageHost = process.env.IMAGE_HOST_URL;
   const prehosted = path.startsWith('http://') || path.startsWith('https://');
@@ -14,18 +23,20 @@ async function sendHostImage(message: CommandInteraction, path: string) {
 }
 
 async function react(interaction: CommandInteraction) {
-  switch (interaction.options.getString('reaction')) {
-    case 'away':
+  const reaction = interaction.options.getString('reaction') as Reaction | null;
+
+  switch (reaction) {
+    case Reaction.away:
       return sendHostImage(interaction, 'memes/away.jpg');
-    case 'boobs':
+    case Reaction.boobs:
       return sendHostImage(interaction, 'memes/boobs.jpg');
-    case 'looking':
+    case Reaction.looking:
       return sendHostImage(interaction, 'memes/looking.jpg');
-    case 'respectfully':
+    case Reaction.respectfully:
       return sendHostImage(interaction, 'memes/respectfully.jpg');
-    case 'shook':
+    case Reaction.shook:
       return sendHostImage(interaction, 'memes/shook.jpg');
-    case 'sheesh':
+    case Reaction.sheesh:
       return sendHostImage(interaction, 'https://cdn.discordapp.com/attachments/736915758558478336/892303098142416896/Asian_Old_Man_Sheesh.mp4');
     default:
       break;
@@ -37,7 +48,7 @@ export default function MemeCommands() {
     {
       handler: react,
       data: new SlashCommandBuilder().setName('react').setDescription('Send a reaction image')
-        .addStringOption((option) => option.setName('reaction').setDescription('The reaction image to send').setRequired(true)),
+        .addStringOption((option) => option.setName('reaction').setDescription(`The reaction image to send. Can be one of: ${Object.keys(Reaction)}`).setRequired(true)),
     },
   ];
 }
