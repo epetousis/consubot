aa5cf73e8002d874e9e591c5dce254b208bd16e6
Evan Petousis <evan@petousis.net>
2021-09-28 16:58:54 +1000

    Add "sheesh" meme

--- src/memes.ts
+++ src/memes.ts
@@ -3,13 +3,14 @@ import { SlashCommandBuilder } from '@discordjs/builders';
 
 async function sendHostImage(message: CommandInteraction, path: string) {
   const imageHost = process.env.IMAGE_HOST_URL;
+  const prehosted = path.startsWith('http://') || path.startsWith('https://');
 
-  if (!imageHost) {
+  if (!imageHost && !prehosted) {
     await message.reply({ content: 'Someone forgot to set an image host for these meme images, so I unfortunately cannot send them. ' });
     return;
   }
 
-  await message.reply({ files: [`${imageHost}/${path}`] });
+  await message.reply({ files: [prehosted ? path : `${imageHost}/${path}`] });
 }
 
 async function react(interaction: CommandInteraction) {
@@ -24,6 +25,8 @@ async function react(interaction: CommandInteraction) {
       return sendHostImage(interaction, 'memes/respectfully.jpg');
     case 'shook':
       return sendHostImage(interaction, 'memes/shook.jpg');
+    case 'sheesh':
+      return sendHostImage(interaction, 'https://cdn.discordapp.com/attachments/736915758558478336/892303098142416896/Asian_Old_Man_Sheesh.mp4');
     default:
       break;
   }
