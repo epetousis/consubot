3b14e47aad0e34f7f0d3776207dde191bec270f1
Evan Petousis <evan@petousis.net>
2021-10-02 23:45:01 +1000

    Add permissions check to bot

--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,23 @@
-import { CommandInteraction } from 'discord.js';
+import { CommandInteraction, Permissions } from 'discord.js';
 import { SlashCommandBuilder } from '@discordjs/builders';
 
 async function spoiler(message: CommandInteraction) {
+  if (!message.channel || !('guild' in message.channel)) {
+    return message.reply({
+      content: 'This command cannot be used in direct messages.',
+      ephemeral: true,
+    });
+  }
+
+  // TODO: make the command parser handle permission detection, and calculate bitwise perms flag
+  const channelPerms = message.guild?.me?.permissionsIn(message.channel);
+  if (!channelPerms?.has(Permissions.FLAGS.MANAGE_MESSAGES)) {
+    return message.reply({
+      content: 'Missing Manage Messages permission. Please tell an admin to add this to my role.',
+      ephemeral: true,
+    });
+  }
+
   const messages = await message.channel?.messages.fetch({ limit: 10, before: message.id });
   const mostRecentMessageFromUser = messages?.find((value) => value.member === message.member);
 
