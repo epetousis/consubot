1217de4c789903bef90b42abfca5d4eb1d889e49
Aditha Doratiyawa <indegamedev@gmail.com>
2022-05-18 10:33:30 +1000

    fixed indentation

--- src/obama.ts
+++ src/obama.ts
@@ -5,17 +5,17 @@ const DISCRIMINATOR = 'obama';
 let mesageCounter = 0;
 
 function getProbability(messages: number) {
-    return Math.min(1, (messages / MAXIMUM_MESSAGES_BETWEEN_OBAMA) ** 3);
+  return Math.min(1, (messages / MAXIMUM_MESSAGES_BETWEEN_OBAMA) ** 3);
 }
 
 export default function setupObama(client: Client) {
-    client.on('messageCreate', (message) => {
-        if (message.content.toLowerCase().includes(DISCRIMINATOR)) {
-            console.log('recieved obama');
-            if (Math.random() < getProbability(++mesageCounter)) {
-                message.channel.send('https://m.youtube.com/watch?v=0aQwnxwnve0');
-                mesageCounter = 0;
-            }
-        }
-    });
+  client.on('messageCreate', (message) => {
+    if (message.content.toLowerCase().includes(DISCRIMINATOR)) {
+      console.log('recieved obama');
+      if (Math.random() < getProbability(++mesageCounter)) {
+        message.channel.send('https://m.youtube.com/watch?v=0aQwnxwnve0');
+        mesageCounter = 0;
+      }
+    }
+  });
 }
