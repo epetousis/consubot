a460fe6a067fb26ac191755d6365e3ae2add6a17
Evan Petousis <evan@petousis.net>
2022-05-19 14:35:57 +1000

    fix: concerns outlined in #18

--- src/textmemes.ts
+++ src/textmemes.ts
@@ -15,27 +15,25 @@ async function reactTextImage(
   yPos: number,
   maxWidth: number,
 ) {
-  Jimp.read(path)
-    .then(async (image) => {
-      const loadedImage = image;
-      const font = await Jimp.loadFont(Jimp.FONT_SANS_16_BLACK);
-      loadedImage.print(font, xPos, yPos, text, maxWidth)
-        .write(`temp/${message.id}.png`);
-    });
+  const image = await Jimp.read(path);
+  const loadedImage = image;
+  const font = await Jimp.loadFont(Jimp.FONT_SANS_64_BLACK);
+  await loadedImage.print(font, xPos, yPos, text, maxWidth)
+    .writeAsync(`/tmp/consubot/${message.id}.png`);
   const tempImgId = message.id;
-  await message.reply({ files: [`temp/${message.id}.png`] })
-    .then(() => {
-      Fs.unlinkSync(`temp/${tempImgId}`);
-    });
+  await message.editReply({ files: [`/tmp/consubot/${message.id}.png`] });
+  Fs.unlinkSync(`/tmp/consubot/${tempImgId}.png`);
 }
 
 async function reactText(interaction: CommandInteraction) {
+  await interaction.deferReply();
+
   const reactionImage = interaction.options.getString('meme') as ReactionImage | null;
   const reactionText = interaction.options.getString('text') as string;
 
   switch (reactionImage) {
     case ReactionImage.Rdj:
-      return reactTextImage(interaction, 'memes/rdj.png', reactionText, 69, 69, 434);
+      return reactTextImage(interaction, 'public/memes/rdj.png', reactionText, 69, 69, 434);
     default:
       return null;
   }
