bba6d166d3b2b37b8340e68da9e9e52fd24f0414
Lucy Dunham <lucy@lucy.link>
2022-05-21 03:08:37 +1000

    Update intents

--- src/bot.ts
+++ src/bot.ts
@@ -36,6 +36,7 @@ const client = new Client({
     Intents.FLAGS.GUILDS,
     Intents.FLAGS.GUILD_MESSAGES,
     Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Intents.FLAGS.GUILD_VOICE_STATES,
   ],
 });
 
